2ccf4c35168d84ecaf033c0914c3b595f7177f22
dailam1209 <lamngocdai050920@gmail.com>
2024-02-20 10:16:50 +0700

    update 20/02

---
+++ commons/modal/modal.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import {  Button, Space  } from 'antd'
+import Modal from "antd";
+
+export interface ModalProps {
+    openModalAddToGroup: any
+    setOpenModalAddToGroup: any,
+    text: string
+}
+export const ModalCustom: React.FC<ModalProps> = ({ openModalAddToGroup, setOpenModalAddToGroup, text }) => {
+    const [isChecked, setIsChecked] = useState(false);
+    const [openModalSucess, setOpenModalSucess] = useState(false)
+
+    // const handleCheckAllChange = (e) => {
+    //     setIsChecked(e.target.checked);
+    // };
+
+    // const onFinish = (value) => {
+    //     setOpenModalAddToGroup(false)
+    //     setOpenModalSucess(true)
+    // }
+    return (
+
+            <Modal
+                width={671}
+                open={openModalAddToGroup}
+                onCancel={() => {
+                    // setOpenModalAddToGroup(false)
+                }}
+                footer={null}
+            >
+                <div className={``}>
+                    <div className={``} onClick={() => { 
+                        // setOpenModalAddToGroup(false)
+                         }}>
+                            <p>sdadfasd</p>
+                    </div>
+
+                </div>
+                <div className={``}>
+                    
+                    <div className={}>
+                        <div style={{ flex: 1 }}>
+                            <Button
+                                size="large"
+                                type="primary"
+                                htmlType="submit"
+                                style={{ background: '#4C5BD4' }}
+                            >
+                                Mời vào nhóm
+                            </Button>
+                        </div>
+                        <div style={{ flex: 1 }}>
+                            <Button
+                                style={{ background: '#D44C4C' }}
+                                size="large"
+                                onClick={() => { 
+                                    // setOpenModalAddToGroup(false) 
+                                    }}>
+                                Huỷ
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+    )
+}
+
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { LocationOfSite } from "@/component/interface";
 import Layout from "@/component/layout/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Context, createContext, useContext, useState } from "react";
+import { Context, createContext, useContext, useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import NavMobile from "./photo/commons/navbarMobile/navMobile";
 
@@ -18,17 +18,46 @@ export default function App({ Component, pageProps }: AppProps) {
   const [locationOfSite, setLocationOfSite] = useState<LocationOfSite>({
     name: "home"
   });
+  const [count, setCount] = useState<number>(0);
   const [isTabSide, setTabSide] = useState<boolean>(true);
   const isMobileCheck: boolean = useMediaQuery({
     query: "(max-width: 430px)"
   });
+  const isTabnetCheck: boolean = useMediaQuery({
+    query: "(max-width: 912px)"
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (count < 1 && window.innerWidth >= 912) {
+        setTabSide(true);
+        return 0;
+      }
+      if (typeof window !== "undefined" && window.innerWidth <= 912) {
+        console.log('here');
+        setTabSide(false);
+        return 0;
+      } else {
+        setTabSide(true);
+      }
+      setCount(1);
+    };
+
+    // if (typeof window !== "undefined") {
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    // }
+  }, []);
   return (
     <LocationContext.Provider value={{ locationOfSite, setLocationOfSite }}>
       <TabSideContext.Provider value={{ isTabSide, setTabSide }}>
         <Layout>
           <>
             <Component {...pageProps} />
-            {isMobileCheck && <NavMobile/>}
+            {isMobileCheck && <NavMobile />}
           </>
         </Layout>
       </TabSideContext.Provider>
